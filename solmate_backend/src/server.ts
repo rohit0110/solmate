d7e253b2e86f293f774046b0c19d935008114f77
rohit0110 <rohit7rathore15@gmail.com>
2025-10-09 14:22:43 +0530

    chore: add skeletal express server

---
+++ solmate_backend/src/server.ts
@@ -0,0 +1,8 @@
+// src/server.ts
+import app from './app.js';
+import config from './config/config.js';
+
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
+});
+
